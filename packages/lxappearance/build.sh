5f5db13c8de2f13fb55dd49cfbdebc8c0a5c864c
suhan-paradkar <12suhangp34@gmail.com>
2021-07-15 06:37:17 +0530

    Add package: lxappearance

---
+++ packages/lxappearance/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://launchpad.net/lxappearance
+TERMUX_PKG_DESCRIPTION="A desktop-independent theme switcher for GTK+"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@suhan-paradkar"
+TERMUX_PKG_VERSION=0.6.3
+TERMUX_PKG_SRCURL=https://github.com/lxde/lxappearance.git
+TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
+TERMUX_PKG_DEPENDS="gtk2"
