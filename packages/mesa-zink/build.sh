4fdb44aabed60fc14ab9f79a89c252d9b53fd231
DLC01 <74525818+DLC01@users.noreply.github.com>
2021-09-05 10:59:56 +0530

    mesa-zink

---
+++ packages/mesa-zink/build.sh
@@ -0,0 +1,49 @@
+TERMUX_PKG_HOMEPAGE=https://www.mesa3d.org
+TERMUX_PKG_DESCRIPTION="An open-source implementation of the OpenGL specification"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="DLC01"
+TERMUX_PKG_VERSION=21.0.0
+TERMUX_PKG_REVISION=2
+TERMUX_PKG_SRCURL=https://github.com/PojavLauncherTeam/mesa/archive/refs/heads/release-${TERMUX_PKG_VERSION}.zip
+TERMUX_PKG_SHA256=66d5db6e1530e0addb473bd54aa8f3d273dc76d1ca6f4dbef1e57afac808b435
+TERMUX_PKG_DEPENDS="libandroid-shmem, bison, flex, libexpat, libdrm, libx11, libxdamage, libxext, zstd, libxml2, libxshmfence, zlib, vulkan-headers, vulkan-loader-android"
+TERMUX_PKG_BUILD_DEPENDS="xorgproto"
+TERMUX_PKG_CONFLICTS="libmesa"
+TERMUX_PKG_REPLACES="libmesa"
+TERMUX_PKG_RM_AFTER_INSTALL="include/KHR/khrplatform.h"
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dgbm=disabled
+-Degl=disabled
+-Dgles1=disabled
+-Dgles2=disabled
+-Dlmsensors=disabled
+-Dglx-direct=true
+-Dllvm=disabled
+-Dglx=dri
+-Ddri3=enabled
+-Dxlib-lease=disabled
+-Dshader-cache=enabled
+-Dplatforms=x11
+-Dopengl=true
+-Dosmesa=true
+-Dvulkan-drivers=
+-Dgallium-drivers=zink
+-Dshared-glapi=enabled
+-Ddri-drivers=
+"
+
+termux_step_pre_configure() {
+	export LDFLAGS+=" -L${TERMUX_PREFIX}/lib -landroid-shmem -latomic -llog "
+}
+
+termux_step_post_massage() {
+	cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/lib || exit 1
+	if [ ! -e "./libGL.so.1" ]; then
+		ln -sf libGL.so libGL.so.1
+	fi
+}
+
+termux_step_install_license() {
+	install -Dm600 -t $TERMUX_PREFIX/share/doc/mesa $TERMUX_PKG_BUILDER_DIR/LICENSE
+}
